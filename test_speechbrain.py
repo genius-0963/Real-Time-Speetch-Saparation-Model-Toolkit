acf1ab10210c75c6a5f64062cfee61bc6b358688
iiitiansaurabh <79242161+genius-0963@users.noreply.github.com>
2025-05-28 14:46:56 +0530

    Initial commit

---
+++ test_speechbrain.py
@@ -0,0 +1,7 @@
+import speechbrain as sb
+
+# Print SpeechBrain version
+print(f"SpeechBrain version: {sb.__version__}")
+
+# Print some basic information about the installation
+print("\nSpeechBrain installation test successful!") 
